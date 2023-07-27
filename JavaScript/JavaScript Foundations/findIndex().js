feadee30f63d67e9d350230447a7fb03f41744c9
Beau <halebeau3@gmail.com>
2023-07-26 19:19:45 -0700

    Added: js files

---
+++ JavaScript/JavaScript Foundations/findIndex().js
@@ -0,0 +1,7 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => animals === 'elephant')
+
+// create variable that finds the first element in array with 'e'
+
+const startsWithS = animals.findIndex(animal => animal[0] === 's')
