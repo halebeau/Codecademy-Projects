25bc6c4f1befa7b1567464601e376cceda4379a4
Beau Hale <rodney.hale@va.gov>
2023-08-31 13:21:12 -0700

    Last test passed

--- JavaScript/JavaScript Intermediate/lodash/_.js
+++ JavaScript/JavaScript Intermediate/lodash/_.js
@@ -76,8 +76,16 @@ const _ = {
       return droppedArray
   },
 
-  chunk() {
-
+  chunk(array, size) {
+    if (size === undefined) {
+      size = 1;
+    }
+    const arrayChunks = [];
+    for (let i=0; i < array.length; i+= size) {
+      let arrayChunk = array.slice(i, i + size);
+      arrayChunks.push(arrayChunk)
+    }
+    return arrayChunks
   }
 };
 
